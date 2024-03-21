51a70fb62a07a0d41d5d51da42bed89f8458e35f
JakeClark-chan <22521350@gm.uit.edu.vn>
2024-03-21 10:02:06 +0700

    LocalStorage CRUD Demo

--- static/js/app.js
+++ static/js/app.js
@@ -1,36 +1,87 @@
-// This is the main file for the app
-// using jQuery
-// Toggle cogwheel icon on click
+// jQuery code for changing title ID based on 
+/*
+from
+<div id="carousel-item-2" class="ease-in-out duration-700 green task-container border-primary-green absolute inset-0 transition-transform transform translate-x-full z-20" data-carousel-item="">
+                    
+                </div>
+<div id="carousel-item-2" class="ease-in-out duration-700 green task-container border-primary-green absolute inset-0 transition-transform transform z-20 translate-x-0 z-30" data-carousel-item="">
+                    
+                </div>
+<div id="carousel-item-2" class="ease-in-out duration-700 green task-container border-primary-green absolute inset-0 transition-transform transform z-30 -translate-x-full z-10" data-carousel-item="">
+                    
+                </div>
+<div id="carousel-item-2" class="ease-in-out duration-700 green task-container border-primary-green absolute inset-0 transition-transform transform z-30 -translate-x-full z-10 hidden" data-carousel-item="">
+                    
+                </div>
+
+*/
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+// Find index of div[data-carousel-item] that has class "z-30", which is visible on the screen
+async function findIndexOfCarouselItem (){
+  // let index = 0;
+  // const carouselItems = $('[data-carousel-item]');
+  // for (let i = 0; i < carouselItems.length; i++) {
+  //   if (carouselItems.eq(i).hasClass('translate-x-0')) {
+  //     index = i;
+  //     console.log(i);
+  //     break;
+  //   }
+  // }
+  // console.log(index);
+  // return index;
+  await sleep(500);
+  console.log($('[data-carousel-item]').index($(".translate-x-0")));
+  console.log(typeof($('[data-carousel-item]').index($(".translate-x-0"))));
+  return $('[data-carousel-item]').index($(".translate-x-0"));
+}
+
+// Update title ID function
+const updateTitle = (index) => {
+  let title = "";
+  let color = "";
+  switch (index){
+    case 0:{
+      title = "Do";
+      color = "bg-primary-red";
+      break;
+    }
+    case 1: {
+      title = "Schedule";
+      color = "bg-primary-green";
+      break;
+    }
+    case 2: {
+      title = "Delegate";
+      color = "bg-primary-blue";
+      break;
+    }
+    case 3: {
+      title = "Delete";
+      color = "bg-primary-yellow";
+      break;
+    }
+    default: // Fallback, set to index of active
+      title = "???";         
+  }
+  $('#carousel-title').text(title);
+  // Remove all classes starting with "bg-primary"
+  $('#carousel-hr').removeClass(function(index, className) {
+    // Match classes starting with "bg-primary"
+    console.log(className.match(/\bbg-primary\S+/g) || []);
+    return (className.match(/\bbg-primary\S+/g) || []).join(' ');
+  });
+  $('#carousel-hr').addClass(color);
+}
+
+// document.ready
 $(document).ready(function() {
-  $('.cogwheel').click(function() {
-    // A popup will appear with the settings
-  });
-  // Hide the settings popup when the user clicks outside of it
-  // Later...
+  // updateTitle(0);
 
-  // Slide list of item left
-  $('img.left').click(function() {
-    // Set task container to the left
-    // Create a new left div element
-    var newDiv = $('<div class="green task-container border-green">New Left Div Content</div>');
-    
-    // Add the new div before the "nav" div
-    $('.nav').after(newDiv);
-
-    // Remove the last div that is not the "nav" div
-    $('.lists > div:not(.nav):last').remove();
-  });
-  // Slide list of item right
-  $('img.right').click(function() {
-    // Set task container to the right
-    // Create a new right div element
-    var newDiv = $('<div class="pink task-container border-pink">New Right Div Content</div>');
-    
-    // Add the new div after the "nav" div
-    $('.lists').append(newDiv);
-
-    // Remove the first div that is not the "nav" div
-    $('.lists > div:not(.nav):first').remove();
+  // When button[data-carousel-prev] or button[data-carousel-next] is clicked, change the title ID
+  $('[data-carousel-prev], [data-carousel-next]').on('click', async function() {
+    const index = await findIndexOfCarouselItem();
+    updateTitle(index);
   });
 });
-
